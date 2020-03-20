fbd2a8ae8e7975d14f4c7a8267f30130a0b42d15
Ashley Winters <ashbwinters@gmail.com>
2020-03-20 17:20:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  index = 0
+  while index < array.length do
+    new_array.push(yield(array[index]))
+    index += 1
+  end
+  new_array
+end
+
+def reduce(array, starting_point = 0)
+  answer = 0
+  while starting_point < array.length do
+    yield(array[starting_point])
+    starting_point += 1
+  end
+  answer
+end
+
